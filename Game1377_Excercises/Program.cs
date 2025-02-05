8b5eb30d5326bcc70107a8249c550e38dccee42b
dxcso <dxcso@DESKTOP-0OFSMPT>
2025-02-04 18:12:03 -0600

    Started by adding the arithmetic code and operators first and displaying them idividually. Then wrote to the console all the Outputs variations of the strings. After that I moved on to the Conditionals and comparisons and wrote 4 different conditions with an else statement at the end if none of them applied to my variables.

--- Game1377_Excercises/Program.cs
+++ Game1377_Excercises/Program.cs
@@ -1,9 +1,10 @@
 ﻿namespace Game1377_Excercises;
 
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is Game1377");
     }
 }
